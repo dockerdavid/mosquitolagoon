303f4ee76d686f542b233e8989fd95cf37df9d08
David Gomez <david.gomez@amovil.co>
2024-06-15 17:36:41 -0500

    agregar el clamp

--- src/components/section2/Section2.jsx
+++ src/components/section2/Section2.jsx
@@ -3,21 +3,23 @@ import './section2.css'
 const Section2 = () => {
 	return (
 		<div className='gridContainerSection2'>
-			<div className='cardDetail'>
-				<h2 className='amountDetail'>75</h2>
-				<span className='nameDetail'>Years Experience</span>
-			</div>
-			<div className='cardDetail'>
-				<h2 className='amountDetail'>1.709</h2>
-				<span className='nameDetail'>Happy Clients</span>
-			</div>
-			<div className='cardDetail'>
-				<h2 className='amountDetail'>25</h2>
-				<span className='nameDetail'>Trainers</span>
-			</div>
-			<div className='cardDetail'>
-				<h2 className='amountDetail'>27</h2>
-				<span className='nameDetail'>Award Winning</span>
+			<div className='cards'>
+				<div className='cardDetail'>
+					<h2 className='amountDetail'>75</h2>
+					<span className='nameDetail'>Years Experience</span>
+				</div>
+				<div className='cardDetail'>
+					<h2 className='amountDetail'>1.709</h2>
+					<span className='nameDetail'>Happy Clients</span>
+				</div>
+				<div className='cardDetail'>
+					<h2 className='amountDetail'>25</h2>
+					<span className='nameDetail'>Trainers</span>
+				</div>
+				<div className='cardDetail'>
+					<h2 className='amountDetail'>27</h2>
+					<span className='nameDetail'>Award Winning</span>
+				</div>
 			</div>
 		</div>
 	)
