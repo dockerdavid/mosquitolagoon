ed09c52047c79731e99c29b35cab15d4206732c0
David Gomez <david.gomez@amovil.co>
2024-06-15 16:08:38 -0500

    agregar tarjetas de servicios y diseño responsive

--- src/components/section4/Section4.jsx
+++ src/components/section4/Section4.jsx
@@ -4,6 +4,45 @@ const Section4 = () => {
 	return (
 		<div>
 			<h2>section4</h2>
+
+			<div className='gridContainer'>
+			<div className='service'>
+					<img src='section4/1.jpg' alt='service' />
+					<h3 className='serviceTitle'>Children Lessons</h3>
+					<span className='serviceDescription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam qui corporis maxime eveniet, quidem aspernatur voluptatum dolorum dolorem eaque ab?</span>
+					<button className='serviceButton'>Book Now</button>
+				</div>
+				<div className='service'>
+					<img src='section4/1.jpg' alt='service' />
+					<h3 className='serviceTitle'>Children Lessons</h3>
+					<span className='serviceDescription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam qui corporis maxime eveniet, quidem aspernatur voluptatum dolorum dolorem eaque ab?</span>
+					<button className='serviceButton'>Book Now</button>
+				</div>
+				<div className='service'>
+					<img src='section4/1.jpg' alt='service' />
+					<h3 className='serviceTitle'>Children Lessons</h3>
+					<span className='serviceDescription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam qui corporis maxime eveniet, quidem aspernatur voluptatum dolorum dolorem eaque ab?</span>
+					<button className='serviceButton'>Book Now</button>
+				</div>
+				<div className='service'>
+					<img src='section4/1.jpg' alt='service' />
+					<h3 className='serviceTitle'>Children Lessons</h3>
+					<span className='serviceDescription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam qui corporis maxime eveniet, quidem aspernatur voluptatum dolorum dolorem eaque ab?</span>
+					<button className='serviceButton'>Book Now</button>
+				</div>
+				<div className='service'>
+					<img src='section4/1.jpg' alt='service' />
+					<h3 className='serviceTitle'>Children Lessons</h3>
+					<span className='serviceDescription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam qui corporis maxime eveniet, quidem aspernatur voluptatum dolorum dolorem eaque ab?</span>
+					<button className='serviceButton'>Book Now</button>
+				</div>
+				<div className='service'>
+					<img src='section4/1.jpg' alt='service' />
+					<h3 className='serviceTitle'>Children Lessons</h3>
+					<span className='serviceDescription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam qui corporis maxime eveniet, quidem aspernatur voluptatum dolorum dolorem eaque ab?</span>
+					<button className='serviceButton'>Book Now</button>
+				</div>
+			</div>
 		</div>
 	)
 }
