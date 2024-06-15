4060c4275f9cf2680bf4cac3bac18938f0ca3993
David Gomez <david.gomez@amovil.co>
2024-06-15 17:29:18 -0500

    sección 2 terminada

--- src/components/section2/Section2.jsx
+++ src/components/section2/Section2.jsx
@@ -2,8 +2,23 @@ import './section2.css'
 
 const Section2 = () => {
 	return (
-		<div>
-			<h2>section2</h2>
+		<div className='gridContainerSection2'>
+			<div className='cardDetail'>
+				<h2 className='amountDetail'>75</h2>
+				<span className='nameDetail'>Years Experience</span>
+			</div>
+			<div className='cardDetail'>
+				<h2 className='amountDetail'>1.709</h2>
+				<span className='nameDetail'>Happy Clients</span>
+			</div>
+			<div className='cardDetail'>
+				<h2 className='amountDetail'>25</h2>
+				<span className='nameDetail'>Trainers</span>
+			</div>
+			<div className='cardDetail'>
+				<h2 className='amountDetail'>27</h2>
+				<span className='nameDetail'>Award Winning</span>
+			</div>
 		</div>
 	)
 }
--- src/components/section4/Section4.jsx
+++ src/components/section4/Section4.jsx
@@ -3,10 +3,8 @@ import './section4.css'
 const Section4 = () => {
 	return (
 		<div className='containerSection4'>
-			<h2>section4</h2>
-
 			<div className='gridContainer'>
-			<div className='service'>
+				<div className='service'>
 					<img src='section4/1.jpg' alt='service' />
 					<h3 className='serviceTitle'>Children Lessons</h3>
 					<span className='serviceDescription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam qui corporis maxime eveniet, quidem aspernatur voluptatum dolorum dolorem eaque ab?</span>
