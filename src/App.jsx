d0bd64b8bce43bd1ce78e0195a69971a55b65ce4
Duban Caballero <duban.caballero@amovil.co>
2024-06-15 14:23:09 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './globals.css'
+
+const App = () => {
+	return (
+		<div style={{
+			height: '100vh',
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+		}}>
+			<h1>Base</h1>
+		</div>
+	)
+}
+
+export default App
