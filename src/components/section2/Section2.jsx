d94c313711450799d21238eae039c6440a0a5bfd
David Gomez <david.gomez@amovil.co>
2024-06-17 19:28:17 -0500

    elemento fixed

--- src/components/section2/Section2.jsx
+++ src/components/section2/Section2.jsx
@@ -2,25 +2,8 @@ import './section2.css'
 
 const Section2 = () => {
 	return (
-		<div className='gridContainerSection2'>
-			<div className='cards'>
-				<div className='cardDetail'>
-					<h2 className='amountDetail'>75</h2>
-					<span className='nameDetail'>Years Experience</span>
-				</div>
-				<div className='cardDetail'>
-					<h2 className='amountDetail'>1.709</h2>
-					<span className='nameDetail'>Happy Clients</span>
-				</div>
-				<div className='cardDetail'>
-					<h2 className='amountDetail'>25</h2>
-					<span className='nameDetail'>Trainers</span>
-				</div>
-				<div className='cardDetail'>
-					<h2 className='amountDetail'>27</h2>
-					<span className='nameDetail'>Award Winning</span>
-				</div>
-			</div>
+		<div>
+			<h2>section2</h2>
 		</div>
 	)
 }
--- src/components/section3/Section3.jsx
+++ src/components/section3/Section3.jsx
@@ -2,8 +2,25 @@ import './section3.css'
 
 const Section3 = () => {
 	return (
-		<div>
-			<h2>section3</h2>
+		<div className='gridContainerSection2'>
+			<div className='cards'>
+				<div className='cardDetail'>
+					<h2 className='amountDetail'>75</h2>
+					<span className='nameDetail'>Years Experience</span>
+				</div>
+				<div className='cardDetail'>
+					<h2 className='amountDetail'>1.709</h2>
+					<span className='nameDetail'>Happy Clients</span>
+				</div>
+				<div className='cardDetail'>
+					<h2 className='amountDetail'>25</h2>
+					<span className='nameDetail'>Trainers</span>
+				</div>
+				<div className='cardDetail'>
+					<h2 className='amountDetail'>27</h2>
+					<span className='nameDetail'>Award Winning</span>
+				</div>
+			</div>
 		</div>
 	)
 }
